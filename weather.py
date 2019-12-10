04b01f47966a45135cc4b9af45bd04726c1e0a07
ludd <ludd@chovanec.com>
2019-12-10 19:35:57 +0100

    More comments

--- weather.py
+++ weather.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+# Functional prototype for logging weather data from public API to SQL database
+# an displaying history
+
 DB_FILE = "weather.sqlite3"
 LOG_FILE = "weather.log"
 PERIOD = 5 # in minutes
@@ -5,13 +10,16 @@ LOCATION_NAME = "Ostrava"
 LAT_LON = "49.8209,18.2625"
 API_KEY = "396d860ffc2d16b44404698531c49a1e"
 
+# load simple app server to publish script as web service
 from flask import Flask, render_template
 app = Flask(__name__, static_url_path='', template_folder=".")
 
+# setup logging
 import logging
 logging.basicConfig(filename=LOG_FILE, format='%(levelname)s: %(asctime)s - %(message)s', \
 					datefmt='%y-%b-%d %H:%M:%S')
 
+# we'll use SQLite as database
 import sqlite3
 from sqlite3 import Error
 
@@ -23,13 +31,16 @@ import requests
 from requests.exceptions import HTTPError
 
 def error(msg):
-	# handle errors - just log for now
+	"""This function should send notifications in case of problems"""
+	# just log for now
 	logging.error(msg)
 	
 def shutdown():
+	"""Gracefully end the script and Flask app server """
 	raise RuntimeError("Server going down")
 	
 def get_data():
+	"""This function gets called by the scheduler to get data from API provider and save it to the db"""
 	# request from darksky
 	url = f"https://api.darksky.net/forecast/{API_KEY}/{LAT_LON}?exclude=hourly,minutely,daily,alerts&units=si&lang=cs"
 	try:
@@ -40,7 +51,7 @@ def get_data():
 	except Exception as e:
 		error(e)
 	else:
-		# save in the database
+		# save to the database
 		try:
 			data = response.json()
 			# TODO: data validation
@@ -56,6 +67,7 @@ def get_data():
 			
 
 def exec_sql(sql):
+	"""Execute SQL command"""
 	conn = sqlite3.connect(DB_FILE)
 	c = conn.cursor()
 	c.execute(sql)
@@ -63,6 +75,7 @@ def exec_sql(sql):
 	conn.close()
 	
 def fetch_sql(sql):
+	"""Execute SQL command and return data"""
 	conn = sqlite3.connect(DB_FILE)
 	c = conn.cursor()
 	c.execute(sql)
@@ -86,6 +99,7 @@ def create_db():
 		shutdown()
 
 def setup():
+	"""Initial setup"""
 	# create database if it doesn't exist
 	if not os.path.isfile(DB_FILE):
 		create_db()
@@ -97,10 +111,12 @@ def setup():
 
 @app.template_filter('format_datetime')
 def format_datetime_filter(s):
-    return datetime.datetime.fromtimestamp(int(s)).strftime('%Y-%m-%d %H:%M:%S')
+	"""Filter used in the template to format date and time"""
+	return datetime.datetime.fromtimestamp(int(s)).strftime('%Y-%m-%d %H:%M:%S')
 
 @app.route('/')
 def show_all():
+	"""This function gets called when an http request comes"""
 	# get data
 	sql = "select * from weather_log order by id desc"
 	rows = fetch_sql(sql)
